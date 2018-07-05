0703f085c419734a3e9de589435db54bfa233afe
LonleyC <ananthu.kanive6@gmail.com>
2018-07-05 22:53:30 +0530

    improvements to author page

--- app/src/main/java/com/example/ananthu/getbooks3/AuthorViewActivity.java
+++ app/src/main/java/com/example/ananthu/getbooks3/AuthorViewActivity.java
@@ -1,25 +1,33 @@
 package com.example.ananthu.getbooks3;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class AuthorViewActivity extends AppCompatActivity {
+public class AuthorViewActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
 
 
     private ImageView authorImage;
     private TextView authorName;
     private TextView about;
+    private TextView webAbout;
     private List<Book> books = new ArrayList<>();
 
     private RecyclerView recyclerView;
@@ -31,7 +39,6 @@ public class AuthorViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_author_view);
-
 
         authorName = findViewById(R.id.firstLine);
         authorImage = findViewById(R.id.authorImage);
@@ -48,15 +55,27 @@ public class AuthorViewActivity extends AppCompatActivity {
         mAdapter = new BookRecyclerViewAdapter(books);
         recyclerView.setAdapter(mAdapter);
 
+        webAbout = findViewById(R.id.webAbout);
+        ToggleButton aboutToggle = findViewById(R.id.aboutToggle);
+        ToggleButton bookToggle = findViewById(R.id.bookToggle);
+        ToggleButton favoriteToggle = findViewById(R.id.favorite_toggle);
+
+        aboutToggle.setOnCheckedChangeListener(this);
+        bookToggle.setOnCheckedChangeListener(this);
+        favoriteToggle.setOnCheckedChangeListener(this);
+
         mGoodreadRequest = new GoodreadRequest(getString(R.string.GR_API_Key), this);
         final Author author = (Author) getIntent().getSerializableExtra("author");
-
+        final ScrollView content = findViewById(R.id.content);
+        final ProgressBar loadingIcon = findViewById(R.id.loading_icon);
 
         mGoodreadRequest.getAuthor(author.getId(), new SuccessFailedCallback() {
             @Override
             public void success(String response) {
                 author.getFullDetails(response);
                 updateDetails(author);
+                content.setVisibility(View.VISIBLE);
+                loadingIcon.setVisibility(View.GONE);
             }
 
             @Override
@@ -88,12 +107,41 @@ public class AuthorViewActivity extends AppCompatActivity {
 
                 @Override
                 public void failed() {
-                    Toast.makeText(
-                            getApplicationContext(),
-                            "some error occurred",
-                            Toast.LENGTH_SHORT).show();
+                   Log.e("request", "error getting book from id");
                 }
             });
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if(isChecked){
+            if(buttonView.getId() == R.id.descriptionToggle){
+                buttonView.setCompoundDrawablesWithIntrinsicBounds
+                        (R.drawable.ic_baseline_keyboard_arrow_down_24px, 0, 0, 0);
+                webAbout.setVisibility(View.VISIBLE);
+            } else if(buttonView.getId() == R.id.authorToggle){
+                buttonView.setCompoundDrawablesWithIntrinsicBounds
+                        (R.drawable.ic_baseline_keyboard_arrow_down_24px, 0, 0, 0);
+                recyclerView.setVisibility(View.VISIBLE);
+            } else if(buttonView.getId() == R.id.favorite_toggle){
+                buttonView.setCompoundDrawablesWithIntrinsicBounds
+                        (R.drawable.ic_baseline_stars_gold_14px, 0, 0, 0);
+            }
+        } else {
+
+            if(buttonView.getId() == R.id.descriptionToggle){
+                buttonView.setCompoundDrawablesWithIntrinsicBounds
+                        (R.drawable.ic_baseline_keyboard_arrow_right_24px, 0, 0, 0);
+                webAbout.setVisibility(View.GONE);
+            } else if(buttonView.getId() == R.id.authorToggle){
+                buttonView.setCompoundDrawablesWithIntrinsicBounds
+                        (R.drawable.ic_baseline_keyboard_arrow_right_24px, 0, 0, 0);
+                recyclerView.setVisibility(View.GONE);
+            } else if(buttonView.getId() == R.id.favorite_toggle){
+                buttonView.setCompoundDrawablesWithIntrinsicBounds
+                        (R.drawable.ic_baseline_stars_grey_14px, 0, 0, 0);
+            }
         }
     }
 }
